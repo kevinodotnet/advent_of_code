294ccd5984f9d48bb15a3e42d6d5f089a3f85b1f
Kevin O'Donnell <kevin.odonnell@shopify.com>
2021-12-06 15:31:38 -0500

    d5p2

--- 2021/day_05.rb
+++ 2021/day_05.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 
+# bundle exec ./day_XX.rb
+
 require 'pry'
 require 'set'
+require 'minitest/autorun'
+require 'minitest/focus'
 
 def test_input
     <<EOF
@@ -25,17 +29,65 @@ end
 class Line 
     attr_accessor :x1, :y1, :x2, :y2
     def initialize(x1, y1, x2, y2)
-        self.x1 = [x1, x2].min
-        self.y1 = [y1, y2].min
-        self.x2 = [x1, x2].max
-        self.y2 = [y1, y2].max
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    end
+    def diagonal?
+        (x2-x1).abs == (y2-y1).abs
+    end
+    def horizontal?
+        y1 == y2
+    end
+    def vertical?
+        x1 == x2
     end
     def horizontal_or_vertical?
-        return true if x1 == x2 || y1 == y2
+        horizontal? || vertical?
+    end
+    def points
+        @points ||= begin
+            x_inc = if x1 == x2
+                0
+            elsif x2 > x1
+                1
+            else
+                -1
+            end
+            y_inc = if y1 == y2
+                0
+            elsif y2 > y1
+                1
+            else
+                -1
+            end
+            count = [x2-x1, y2-y1].map{|i| i.abs}.max
+            @points = (0..count).map do |i|
+                r = {
+                    x: x1 + (x_inc * i),
+                    y: y1 + (y_inc * i)
+                }
+                r
+            end
+            @points
+        end
     end
     def intersects(x, y)
-        raise StandardError, 'unsupported' unless horizontal_or_vertical?
-        (x1..x2).include?(x) && (y1..y2).include?(y)
+        if horizontal_or_vertical?
+            (x1..x2).include?(x) && (y1..y2).include?(y)
+        else
+            (x1..x2).include?(x) && (y1..y2).include?(y) && (x-x1 == y-y1)
+        end
+        # return points.include?({x: x, y: y})
+    end
+    def to_s
+        {
+            x1: x1,
+            y1: y1, 
+            x2: x2, 
+            y2: y2
+        }.to_s
     end
 end
 
@@ -47,18 +99,18 @@ class SolutionA
             @lines << Line.new(match['x1'].to_i, match['y1'].to_i, match['x2'].to_i, match['y2'].to_i)
             raise StandardError, "regex failure: #{d}" unless match
         end
-    end
-    def board_str
-        @board.map{|b| b.map{|c| c == 0 ? '.' : c}.join(" ")}.join("\n")
-    end
-    def solve
         max_x = @lines.map{|l| [l.x1, l.x2]}.flatten.max
         max_y = @lines.map{|l| [l.y1, l.y2]}.flatten.max
-        @lines = @lines.select{|l| l.horizontal_or_vertical? }
         @board = (0..max_y).map do |y|
             (0..max_x).map{|i| 0}
         end
-        @lines.each do |l|
+    end
+    def board_str
+        @board.map{|b| b.map{|c| c == 0 ? '.' : c}.join("")}.join("\n")
+    end
+    def solve
+        @lines = @lines.select{|l| l.horizontal_or_vertical? }
+        @lines.each_with_index do |l, i|
             ((l.y1)..(l.y2)).each do |y|
                 ((l.x1)..(l.x2)).each do |x|
                     if l.intersects(x, y)
@@ -69,15 +121,110 @@ class SolutionA
         end
         @board.flatten.select{|d| d>= 2}.count
     end
+    def lines
+        @lines
+    end
 end
 
 class SolutionB < SolutionA
+    def solve
+        @lines = @lines.select{|l| l.diagonal? || l.horizontal_or_vertical? }
+        @lines.each_with_index do |l, i|
+            l.points.each do |p|
+                @board[p[:y]][p[:x]] += 1
+            end
+        end
+        @board.flatten.select{|d| d>= 2}.count
+    end
 end
 
-puts "#" * 100
-puts "test: #{SolutionA.new(test_input).solve}"
-puts "full: #{SolutionA.new(input).solve}"
+class LineTest < Minitest::Test
+    def test_diagonal
+        assert Line.new(0, 0, 2, 2).diagonal?
+        refute Line.new(0, 0, 2, 1).diagonal?
+        assert Line.new(1, 1, 99, 99).diagonal?
+        refute Line.new(0, 1, 99, 99).diagonal?
+    end
+    
+    def test_points_1
+        expected = [
+            {x: 0, y: 0},
+            {x: 1, y: 1},
+            {x: 2, y: 2},
+        ]
+        assert_points(expected, Line.new(2, 2, 0, 0).points)
+        assert_points(expected, Line.new(0, 0, 2, 2).points)
+    end
+
+    def test_points_1
+        expected = [
+            {x: 0, y: 0},
+            {x: 1, y: 1},
+            {x: 2, y: 2},
+        ]
+        assert_points(expected, Line.new(2, 2, 0, 0).points)
+        assert_points(expected, Line.new(0, 0, 2, 2).points)
+    end
+
+    def test_points_2
+        expected = [{:x=>9, :y=>4}, {:x=>8, :y=>4}, {:x=>7, :y=>4}, {:x=>6, :y=>4}, {:x=>5, :y=>4}, {:x=>4, :y=>4}, {:x=>3, :y=>4}]
+        assert_points(expected, Line.new(9, 4, 3, 4).points)
+    end
+    def test_intersects
+        line = Line.new(0, 0, 2, 2)
+        assert line.intersects(1,1)
+        refute line.intersects(0,2)
+        line = Line.new(0, 1, 0, 5)
+        assert line.intersects(0, 2)
+        refute line.intersects(0, 0)
+        assert line.intersects(0, 1)
+        assert line.intersects(0, 2)
+        assert line.intersects(0, 3)
+        assert line.intersects(0, 4)
+        assert line.intersects(0, 5)
+        refute line.intersects(0, 6)
+    end
+
+    private
+
+    def assert_points(p1, p2)
+        assert_equal p1.count, p2.count
+        p1.each do |a|
+            assert p2.include?(a), message: "#{p1} != #{p2}"
+        end
+    end
+end
+
+class SolutionBTest < Minitest::Test
+    def test_the_test_input
+        expected = <<~EOF
+            1.1....11.
+            .111...2..
+            ..2.1.111.
+            ...1.2.2..
+            .112313211
+            ...1.2....
+            ..1...1...
+            .1.....1..
+            1.......1.
+            222111....
+        EOF
+        s = SolutionB.new(test_input)
+        s.solve
+        assert_equal expected.split("\n").join(""), s.board_str.split("\n").join("")
+    end
+end
 
 # puts "#" * 100
-# puts "test: #{SolutionB.new(test_input).solve}"
-# puts "full: #{SolutionB.new(input).solve}"
+
+
+
+
+
+# puts "test: #{SolutionA.new(test_input).solve}"
+# puts "full: #{SolutionA.new(input).solve}"
+
+# puts "#" * 100
+puts "test: #{SolutionB.new(test_input).solve}"
+puts "full: #{SolutionB.new(input).solve}"
+# puts "#" * 100
