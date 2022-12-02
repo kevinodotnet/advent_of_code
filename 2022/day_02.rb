933fa78d3731a8f12cc0304846d9e0755aa53e53
Kevin O'Donnell <kevin.odonnell@shopify.com>
2022-12-02 09:16:08 -0500

    d2p2

--- 2022/day_02.rb
+++ 2022/day_02.rb
@@ -3,7 +3,7 @@ class Solution < AbstractSolution
     @data.split("\n").map{|l| l.split(" ")}
   end
 
-  def score(round)
+  def score_p1(round)
     outcomes = {
       "A" => { # rock
         "X" => 3 + 1, # rock
@@ -27,10 +27,35 @@ class Solution < AbstractSolution
     outcomes[p1][p2]
   end
 
+  def score_p2(round)
+    outcomes = {
+      "A" => { # rock
+        "X" => 0 + 3, # scissors
+        "Y" => 3 + 1, # rock
+        "Z" => 6 + 2, # paper
+      },
+      "B" => { # paper
+        "X" => 0 + 1, # rock
+        "Y" => 3 + 2, # paper
+        "Z" => 6 + 3, # scissors
+      },
+      "C" => { # scissors
+        "X" => 0 + 2, # paper
+        "Y" => 3 + 3, # scissors
+        "Z" => 6 + 1, # rock
+      },
+    }
+
+    p1 = round[0]
+    p2 = round[1]
+    outcomes[p1][p2]
+  end
+
   def part1
-    parse.map{|r| score(r)}.sum
+    parse.map{|r| score_p1(r)}.sum
   end
 
   def part2
+    parse.map{|r| score_p2(r)}.sum
   end
 end
