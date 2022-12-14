fccdf6a594f891d2c41e248b0e041b8772283aac
Kevin O'Donnell <kevin.odonnell@shopify.com>
2022-12-13 22:02:15 -0500

    d13p2

--- 2022/day_13.rb
+++ 2022/day_13.rb
@@ -7,32 +7,40 @@ class Solution < AbstractSolution
     end
   end
 
-  def compare(left, right, indent = 0)
+  def compare(left, right)
     if left.is_a?(Array) && right.is_a?(Array)
       [left.count, right.count].max.times do |i|
-        return true if left[i].nil?
-        return false if right[i].nil?
+        return 1 if left[i].nil?
+        return -1 if right[i].nil?
         result = compare(left[i], right[i])
-        return result unless result.nil?
+        return result unless result == 0
       end
     elsif left.is_a?(Integer) && right.is_a?(Integer)
-      return true if left < right
-      return false if left > right
-      return nil
+      return 1 if left < right
+      return -1 if left > right
+      return 0
     else
       left = [left] if left.is_a?(Integer)
       right = [right] if right.is_a?(Integer)
       return compare(left, right)
     end
-    nil
+    0
   end
 
   def part1
     results = parse.map { |pair| compare(pair[0], pair[1]) }
-    results.map.with_index{|r, i| r ? i+1 : nil}.compact.sum
+    results.map.with_index{|r, i| r == 1 ? i+1 : nil}.compact.sum
   end
 
   def part2
-    # parse
+    packets = []
+    packets << [[2]]
+    packets << [[6]]
+    parse.each do |p|
+      packets << p[0]
+      packets << p[1]
+    end
+    sorted_packets = packets.sort{|a, b| compare(b, a)}
+    (sorted_packets.find_index([[2]]) + 1) * (sorted_packets.find_index([[6]]) + 1)
   end
 end
