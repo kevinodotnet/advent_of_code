7b33e299a76f56495bfc96ba019b3482edda436e
Kevin O'Donnell <kevin.odonnell@shopify.com>
2023-12-07 14:37:03 -0500

    WIP

--- 2023/day_05.rb
+++ 2023/day_05.rb
@@ -45,8 +45,11 @@ class Solution < AbstractSolution
   end
 
   def transform_range_via_map(r, m)
+    m[:range] = (m[:src]..(m[:src]+(m[:size]-1)))
+    puts "transform_range_via_map: r: #{r} m: #{m}"
     # case: r is contained entirely in [m]; just shift according to :dest
     if m[:range].cover?(r)
+      puts "  cover!"
       diff = m[:dest] - m[:src]
       return [
         ((r.first+diff)..(r.last+diff))
@@ -55,6 +58,7 @@ class Solution < AbstractSolution
 
     # case: the two ranges don't touch
     unless m[:range].overlaps?(r)
+      puts "  no_shift!"
       return [r]
     end
 
@@ -64,19 +68,26 @@ class Solution < AbstractSolution
     r1 = r.first
     r2 = nil
     loop do
-      r2 = [
-        m[:range].first,
-        m[:range].first + m[:size],
-        r.last,
-      ].select{|r| r > r1}.min
-      break if r2.nil?
-      range_end = r2 == r.last ? r2 : (r2-1)
-      ranges << (r1..range_end)
-      r1 = r2
+      r2 = if m[:range].include?(r1)
+        [m[:range].last, r.last].min
+      else
+        # binding.pry if [m[:range].first-1, r.last].select{|o| o > r1}.empty?
+        [m[:range].first-1, r.last].select{|o| o > r1 || o == r.last}.min
+      end
+      ranges << (r1..r2)
+      #binding.pry
+      break if r2 == r.last
+      if r1 == r2
+        r1 += 1
+      else
+        r1 = r2 + 1
+      end
       r2 = nil
     end
 
-    ranges.map{|r| m[:range].cover?(r) ? transform_range_via_map(r, m) : r}.flatten
+    result = ranges.map{|r| m[:range].cover?(r) ? transform_range_via_map(r, m) : r}.flatten
+
+    result
   end
 
   def part2
@@ -85,13 +96,11 @@ class Solution < AbstractSolution
     layers = @mappings.values.map do |m|
       m.values.first.to_a.map do |r|
         r[:size] = r[:range]
-        r[:range] = (r[:src]..(r[:src]+r[:size]))
       end
       m.values.first.to_a.sort{|a, b| a[:src] <=> b[:src]}
     end
 
-    @seeds.each_slice(2).map do |num, t|
-      puts "num: #{num}"
+    rr = @seeds.each_slice(2).map do |num, t|
       ranges = [
         (num..(num+t))
       ]
@@ -105,7 +114,12 @@ class Solution < AbstractSolution
         end
       end
 
+      puts ranges.sort{|a, b| a.first <=> b.first}
+      binding.pry if ranges.sort{|a, b| a.first <=> b.first}.first.first == 0
       ranges.sort{|a, b| a.first <=> b.first}.first.first
-    end.min
+    end.compact
+
+    puts rr
+    rr.min
   end
 end
--- 2023/day_05_test.rb
+++ 2023/day_05_test.rb
@@ -45,32 +45,68 @@ class SolutionTest < Minitest::Test
         assert_equal 650599855, Solution.new(data: real_input).part1
     end
 
-    def test_transform_range_via_map
+    def test_transform_range_via_map1
         s = Solution.new(data: SAMPLE_INPUT)
         s.parse
 
         expected = [(81..95)]
-        assert_equal expected, s.transform_range_via_map(79..93, {:src=>50, :dest=>52, :range=>50..97, :size=>48})
+        assert_equal expected, s.transform_range_via_map(79..93, {:src=>50, :dest=>52, :size=>48})
+    end
+
+    def test_transform_range_via_map1
+        s = Solution.new(data: SAMPLE_INPUT)
+        s.parse
 
         expected = [(10..20)]
-        assert_equal expected, s.transform_range_via_map(10..20, {:src=>50, :dest=>52, :range=>50..97, :size=>48})
+        assert_equal expected, s.transform_range_via_map(10..20, {:src=>50, :dest=>52, :size=>48})
+    end
+
+    #focus
+    def test_transform_range_via_map1
+        s = Solution.new(data: SAMPLE_INPUT)
+        s.parse
 
         expected = [
             (40..49),
             (100..101),
             (52..60),
         ]
-        assert_equal expected, s.transform_range_via_map(40..60, {:src=>50, :dest=>100, :range=>50..51, :size=>2})
+        assert_equal expected, s.transform_range_via_map(40..60, {:src=>50, :dest=>100, :size=>2})
+    end
 
+    #focus
+    def test_transform_range_via_map2
+        s = Solution.new(data: SAMPLE_INPUT)
+        s.parse
         expected = [
             (110..119),
-            (50..60)
+            (30..30)
         ]
-        assert_equal expected, s.transform_range_via_map(40..60, {:src=>30, :dest=>100, :range=>30..49, :size=>20})
+        assert_equal expected, s.transform_range_via_map(20..30, {:src=>10, :dest=>100, :size=>20})
+    end
+
+    def test_transform_range_via_map3
+        s = Solution.new(data: SAMPLE_INPUT)
+        s.parse
+            expected = [
+            (40..59),
+            (100..100)
+        ]
+        assert_equal expected, s.transform_range_via_map(40..60, {:src=>60, :dest=>100, :size=>20})
+    end
+
+    def test_transform_range_via_map4
+        s = Solution.new(data: SAMPLE_INPUT)
+        s.parse
+            expected = [
+            (119..119),
+            (80..80)
+        ]
+        assert_equal expected, s.transform_range_via_map(79..80, {:src=>60, :dest=>100, :size=>20})
     end
 
     def test_part2
-        #assert_equal 46, Solution.new(data: SAMPLE_INPUT).part2
+        assert_equal 46, Solution.new(data: SAMPLE_INPUT).part2
         assert_equal 456, Solution.new(data: real_input).part2
     end
 end
