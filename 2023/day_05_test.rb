ce6c8d51ec1276ee67da14868b5da40a4771ceef
Kevin O'Donnell <kevin.odonnell@shopify.com>
2023-12-07 14:51:35 -0500

    WIP

--- 2023/day_05_test.rb
+++ 2023/day_05_test.rb
@@ -45,7 +45,7 @@ class SolutionTest < Minitest::Test
         assert_equal 650599855, Solution.new(data: real_input).part1
     end
 
-    def test_transform_range_via_map1
+    def test_transform_range_via_map1a
         s = Solution.new(data: SAMPLE_INPUT)
         s.parse
 
@@ -53,7 +53,7 @@ class SolutionTest < Minitest::Test
         assert_equal expected, s.transform_range_via_map(79..93, {:src=>50, :dest=>52, :size=>48})
     end
 
-    def test_transform_range_via_map1
+    def test_transform_range_via_map1b
         s = Solution.new(data: SAMPLE_INPUT)
         s.parse
 
@@ -62,7 +62,7 @@ class SolutionTest < Minitest::Test
     end
 
     #focus
-    def test_transform_range_via_map1
+    def test_transform_range_via_map1c
         s = Solution.new(data: SAMPLE_INPUT)
         s.parse
 
@@ -75,7 +75,7 @@ class SolutionTest < Minitest::Test
     end
 
     #focus
-    def test_transform_range_via_map2
+    def test_transform_range_via_map2d
         s = Solution.new(data: SAMPLE_INPUT)
         s.parse
         expected = [
@@ -85,28 +85,82 @@ class SolutionTest < Minitest::Test
         assert_equal expected, s.transform_range_via_map(20..30, {:src=>10, :dest=>100, :size=>20})
     end
 
-    def test_transform_range_via_map3
+    def test_transform_range_via_map3e
         s = Solution.new(data: SAMPLE_INPUT)
         s.parse
-            expected = [
+        expected = [
             (40..59),
             (100..100)
         ]
         assert_equal expected, s.transform_range_via_map(40..60, {:src=>60, :dest=>100, :size=>20})
     end
 
-    def test_transform_range_via_map4
+    def test_transform_range_via_map4f
         s = Solution.new(data: SAMPLE_INPUT)
         s.parse
-            expected = [
+        expected = [
             (119..119),
             (80..80)
         ]
         assert_equal expected, s.transform_range_via_map(79..80, {:src=>60, :dest=>100, :size=>20})
     end
 
+    def test_no_intersect
+        expected = [
+            10..100
+        ]
+        assert_equal expected, test_sol.transform_range_via_map(10..100, {:src=>1000, :dest=>110, :size=>50})
+    end
+
+    def test_range_within_map
+        expected = [
+            115..116
+        ]
+        assert_equal expected, test_sol.transform_range_via_map(1005..1006, {:src=>1000, :dest=>110, :size=>50})
+    end
+
+    def test_range_bigger_map_overlap_start
+        expected = [
+            2005..2009,
+            110..1000
+        ]
+        assert_equal expected, test_sol.transform_range_via_map(105..1000, {:src=>100, :dest=>2000, :size=>10})
+    end
+
+    def test_range_bigger_map_overlap_end
+        expected = [
+            105..994,
+            2000..2005
+        ]
+        assert_equal expected, test_sol.transform_range_via_map(105..1000, {:src=>995, :dest=>2000, :size=>10})
+    end
+
+    def test_range_smaller_map_overlap_start
+        expected = [
+            2100..2199,
+            1100..2000
+        ]
+        assert_equal expected, test_sol.transform_range_via_map(1000..2000, {:src=>900, :dest=>2000, :size=>200})
+    end
+
+    def test_range_smaller_map_overlap_end
+        expected = [
+            2150..2199,
+            1100..2000
+        ]
+        assert_equal expected, test_sol.transform_range_via_map(1050..2000, {:src=>900, :dest=>2000, :size=>200})
+    end
+
     def test_part2
         assert_equal 46, Solution.new(data: SAMPLE_INPUT).part2
-        assert_equal 456, Solution.new(data: real_input).part2
+        assert_equal "not 1549152", Solution.new(data: real_input).part2
+    end
+
+    private
+
+    def test_sol
+        s = Solution.new(data: SAMPLE_INPUT)
+        s.parse
+        s
     end
 end
