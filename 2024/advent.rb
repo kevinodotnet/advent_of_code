1b3d718f7663af85f7ecbaeac4b81ac99fbfbf58
Kevin O'Donnell <kevin.odonnell@shopify.com>
2024-11-25 13:16:05 -0500

    prepare for 2024

---
+++ 2024/advent.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+# bundle exec ./advent.rb
+
+require "active_support"
+require "active_support/core_ext"
+require "benchmark"
+require "pry"
+require "rgl/adjacency"
+require "rgl/dijkstra"
+
+class AbstractSolution
+  def initialize(data:)
+    @data = data
+  end
+end
+
+code_file = Dir["*"].select{|f| f.match(/^day_\d\d\.rb/)}.sort.last
+test_file = code_file.gsub(/\.rb/, '_test.rb')
+input_file = code_file.gsub(/\.rb/, '.input')
+
+load code_file
+
+if ARGV.last == "newday"
+  newday = "%02d" % (code_file.match(/\d+/)[0].to_i + 1)
+  FileUtils.cp("day_00.rb", "day_#{newday}.rb")
+  FileUtils.cp("day_00_test.rb", "day_#{newday}_test.rb")
+  FileUtils.touch("day_#{newday}.input")
+else
+  require "minitest/autorun"
+  require "minitest/focus"
+  load test_file
+end
