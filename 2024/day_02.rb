933091e5b6d185e1d492c6d9baade971f8169ac0
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-12-02 18:28:14 -0500

    y2024d02p02

--- 2024/day_02.rb
+++ 2024/day_02.rb
@@ -3,20 +3,34 @@ class Solution < AbstractSolution
     @data = @data.split("\n").map{|l| l.split(" ").map(&:to_i)}
   end
 
+  def analyze(r)
+    distances = r.each_cons(2).map{|a, b| (b-a)}
+    pos_count = distances.count { |distance| distance.positive? }
+    result = {
+      under_limit: distances.map(&:abs).max <= 3,
+      same_sign: (pos_count == 0 || pos_count == distances.count),
+      no_zeros: distances.count(0) == 0
+    }
+    result.values.all?(true)
+  end
+
   def part1
-    samples = @data.map do |r|
-      distances = r.each_cons(2).map{|a, b| (b-a)}
-      distances.max.abs <= 3
-      pos_count = distances.count { |distance| distance.positive? }
-      {
-        under_limit: distances.map(&:abs).max <= 3,
-        same_sign: (pos_count == 0 || pos_count == distances.count),
-        no_zeros: distances.count(0) == 0
-      }
-    end
-    samples.map{|s| s.values.all?(true)}.count(true)
+    @data.map do |r|
+      analyze(r)
+    end.count(true)
   end
 
   def part2
+    @data.map do |r|
+      if analyze(r)
+        true
+      else
+        result = (r.count).times.map do |i|
+          new_r = r[0...i] + r[i+1..-1]
+          analyze(new_r)
+        end
+        result.count(true) > 0
+      end
+    end.count(true)
   end
 end
--- 2024/day_02_test.rb
+++ 2024/day_02_test.rb
@@ -19,7 +19,7 @@ class SolutionTest < Minitest::Test
     end
 
     def test_part2
-        # assert_equal 456, Solution.new(data: SAMPLE_INPUT_1).part2
-        # assert_equal 456, Solution.new(data: real_input).part2
+        assert_equal 4, Solution.new(data: SAMPLE_INPUT_1).part2
+        assert_equal 366, Solution.new(data: real_input).part2
     end
 end
