cf1a44d361ddd998e6ca5cf67413c1d02f13e564
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-12-03 08:59:12 -0500

    y2024d03p02

--- 2024/day_03.rb
+++ 2024/day_03.rb
@@ -1,13 +1,24 @@
 class Solution < AbstractSolution
   def parse
-    @muls = @data.scan(/mul\(\d+,\d+\)/)
-    @pairs = @muls.map{|o| o.scan(/\d+/).map(&:to_i)}
   end
 
   def part1
+    @muls = @data.scan(/mul\(\d+,\d+\)/)
+    @pairs = @muls.map{|o| o.scan(/\d+/).map(&:to_i)}
     @pairs.map { |a, b| a * b }.sum
   end
 
   def part2
+    doit = true
+    result = 0
+    @data.scan(/mul\(\d+,\d+\)|do\(\)|don't\(\)/).each do |o|
+      if o.match(/mul/)
+        a, b = o.scan(/\d+/).map(&:to_i)
+        result += a * b if doit
+      else
+        doit = o.match(/do\(\)/)
+      end
+    end
+    result
   end
 end
--- 2024/day_03_test.rb
+++ 2024/day_03_test.rb
@@ -3,6 +3,10 @@ class SolutionTest < Minitest::Test
         xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
     EOF
 
+    SAMPLE_INPUT_2 = <<~EOF
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+    EOF
+
     def real_input
         day = __FILE__.match(/\d+/)
         File.read("day_#{day}.input")
@@ -10,11 +14,11 @@ class SolutionTest < Minitest::Test
 
     def test_part1
         assert_equal 161, Solution.new(data: SAMPLE_INPUT_1).part1
-        assert_equal 123, Solution.new(data: real_input).part1
+        assert_equal 170778545, Solution.new(data: real_input).part1
     end
 
     def test_part2
-        # assert_equal 456, Solution.new(data: SAMPLE_INPUT_1).part2
-        # assert_equal 456, Solution.new(data: real_input).part2
+        assert_equal 48, Solution.new(data: SAMPLE_INPUT_2).part2
+        assert_equal 82868252, Solution.new(data: real_input).part2
     end
 end
