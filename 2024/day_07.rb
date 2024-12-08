e7bac9218194f55dc6b64681cb83f011cb62adb3
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-12-08 12:08:51 -0500

    y2024d07p02

--- 2024/day_07.rb
+++ 2024/day_07.rb
@@ -3,8 +3,7 @@ class Solution < AbstractSolution
     @data = @data.split("\n").map{|l| l.scan(/\d+/).map(&:to_i)}
   end
 
-  def part1
-    operations = [:*, :+]
+  def solve(operations)
     r = @data.select do |r|
       e = r.deep_dup
       goal = e.shift
@@ -13,7 +12,11 @@ class Solution < AbstractSolution
         b = e.shift
         values = values.flatten.map do |v|
           operations.map do |op|
-            v.send(op, b)
+            if op == '|'
+              [v,b].join('').to_i
+            else
+              v.send(op, b)
+            end
           end
         end
       end
@@ -22,6 +25,11 @@ class Solution < AbstractSolution
     r.map{|e| e[0]}.sum
   end
 
+  def part1
+    solve([:*, :+])
+  end
+
   def part2
+    solve([:*, :+, '|'])
   end
 end
--- 2024/day_07_test.rb
+++ 2024/day_07_test.rb
@@ -22,7 +22,7 @@ class SolutionTest < Minitest::Test
     end
 
     def test_part2
-        # assert_equal 456, Solution.new(data: SAMPLE_INPUT_1).part2
-        # assert_equal 456, Solution.new(data: real_input).part2
+        assert_equal 11387, Solution.new(data: SAMPLE_INPUT_1).part2
+        assert_equal 97902809384118, Solution.new(data: real_input).part2
     end
 end
