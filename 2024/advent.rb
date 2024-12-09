946e25ca753d34193cd6a72fae6088f90004ea59
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-12-09 06:50:39 -0500

    fix bugs

--- 2024/advent.rb
+++ 2024/advent.rb
@@ -16,19 +16,19 @@ class AbstractSolution
   end
 end
 
-code_file = ARGV.first || Dir["*"].select{|f| f.match(/^day_\d\d\.rb/)}.sort.last
-test_file = code_file.gsub(/\.rb/, '_test.rb')
-input_file = code_file.gsub(/\.rb/, '.input')
+code_file = ARGV.first
 
-load code_file
-
-if ARGV.last == "newday"
+if code_file == "newday"
+  code_file = Dir["*"].select{|f| f.match(/^day_\d\d\.rb/)}.sort.last
   newday = "%02d" % (code_file.match(/\d+/)[0].to_i + 1)
   FileUtils.cp("day_00.rb", "day_#{newday}.rb")
   FileUtils.cp("day_00_test.rb", "day_#{newday}_test.rb")
   FileUtils.touch("day_#{newday}.input")
 else
+  code_file ||= Dir["*"].select{|f| f.match(/^day_\d\d\.rb/)}.sort.last
+  test_file = code_file.gsub(/\.rb/, '_test.rb')
   require "minitest/autorun"
   require "minitest/focus"
+  load code_file
   load test_file
 end
