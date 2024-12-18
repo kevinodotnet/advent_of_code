ae8fbc2eebc26ad84606e92c1974d1feaddf4b06
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-12-17 21:13:03 -0500

    y2024d12p02

--- 2024/day_12.rb
+++ 2024/day_12.rb
@@ -93,7 +93,53 @@ class Solution < AbstractSolution
   def part2
     solve
 
-    binding.pry
-    @crops.sum{|u, c| c[:cost]}
+    # solve for sides, which are fences in the same crop, adjacent with same `dir`
+    # collapse them into `sides` sets, same as for parsing the original crop
+    @crops.each do |uuid, c|
+      c[:sides] = {}
+      DIRS.each do |dir|
+        c[:sides][dir] = {}
+      end
+      c[:fences].each do |p, fs|
+        fs.each do |dir, fenced|
+          next unless fenced
+
+          # check for peers with fences on the same side, and if they are in a [:sides] set. 
+          # If yes, add this fence to the existing side
+          # if no, this fence is the first of a new side
+          peer_dirs = [
+            dir.reverse,
+            dir.reverse.map{|c| c * -1}
+          ]
+          
+          matching_sides = peer_dirs.map do |pd|
+            peer = [p[0] + pd[0], p[1] + pd[1]]
+            
+            matching_side_set = c[:sides][dir].select{|u, s| s.include?(peer)}
+            matching_side_set.values.first
+          end.compact
+
+          merged = false
+          if matching_sides.any?
+            merged = true
+            matching_sides[0] << p
+            if matching_sides.count > 1
+              matching_sides[0] += matching_sides[1]
+              matching_sides[1].clear
+            end
+          end
+
+          unless merged
+            side_id = SecureRandom.uuid
+            c[:sides][dir][side_id] = Set.new
+            c[:sides][dir][side_id] << p
+          end
+        end
+      end
+      c[:side_count] = c[:sides].values.map{|s| s.values}.flatten.select{|s| s.count > 0}.count
+      c[:cost2] = c[:area] * c[:side_count]
+    end
+
+    @crops.sum{|u, c| c[:cost2]}
   end
 end
--- 2024/day_12_test.rb
+++ 2024/day_12_test.rb
@@ -49,7 +49,6 @@ class SolutionTest < Minitest::Test
         File.read("day_#{day}.input")
     end
 
-    focus
     def test_part1
         assert_equal 140, Solution.new(data: SAMPLE_INPUT_1).part1
         assert_equal 772, Solution.new(data: SAMPLE_INPUT_3).part1
@@ -57,12 +56,78 @@ class SolutionTest < Minitest::Test
         assert_equal 1461752, Solution.new(data: real_input).part1
     end
 
-    # focus
+    def test_easy1
+        input = <<~EOF
+            x
+        EOF
+        # assert_equal 4, Solution.new(data: input).part2
+    end
+
+    def test_easy1
+            input = <<~EOF
+            xxx
+        EOF
+        assert_equal 3*4, Solution.new(data: input).part2
+    end
+
+    def test_easy2
+        input = <<~EOF
+        xx
+        xx
+        EOF
+        assert_equal 4*4, Solution.new(data: input).part2
+    end
+
+    def test_easy3
+        input = <<~EOF
+        xxx
+        xxx
+        EOF
+        assert_equal 6*4, Solution.new(data: input).part2
+    end
+
+    def test_easy3
+        input = <<~EOF
+        xxx
+        xxx
+        xxx
+        aaa
+        aaa
+        aaa
+        EOF
+        assert_equal 9*4*2, Solution.new(data: input).part2
+    end
+
+    def test_easy4
+        input = <<~EOF
+            EE
+            EX
+        EOF
+        expected = [
+            3 * 6,
+            1 * 4
+        ].sum
+        assert_equal expected, Solution.new(data: input).part2
+    end
+
+    def test_easy5
+        input = <<~EOF
+            xxx
+            xax
+            xxx
+        EOF
+        expected = [
+            8 * 8,
+            1 * 4
+        ].sum
+        assert_equal expected, Solution.new(data: input).part2
+    end
+
     def test_part2
         assert_equal 236, Solution.new(data: SAMPLE_INPUT_4).part2
-        # assert_equal 368, Solution.new(data: SAMPLE_INPUT_5).part2
-        # assert_equal 1206, Solution.new(data: SAMPLE_INPUT_2).part2
-        # assert_equal 436, Solution.new(data: SAMPLE_INPUT_3).part2
-        # assert_equal 456, Solution.new(data: real_input).part2
+        assert_equal 368, Solution.new(data: SAMPLE_INPUT_5).part2
+        assert_equal 1206, Solution.new(data: SAMPLE_INPUT_2).part2
+        assert_equal 436, Solution.new(data: SAMPLE_INPUT_3).part2
+        assert_equal 904114, Solution.new(data: real_input).part2
     end
 end
